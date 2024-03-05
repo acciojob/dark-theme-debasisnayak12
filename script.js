4eb5722124cd41ab4e0394817812a4e2b0906180
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2024-03-05 07:31:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here
-
+function swapTheme() {
+	const appDiv = document.getElementById("app");
+	const btn = document.getElementById("swap");
+	if(appDiv.classList.contains("day")){
+		appDiv.classList.remove("day");
+		appDiv.classList.add("night");
+		btn.classList.remove("button_day");
+		btn.classList.add("button_night");
+	}else{
+		appDiv.classList.remove("night");
+		appDiv.classList.add("day");
+		btn.classList.remove("button_night");
+		btn.classList.add("button_day");
+	}
+}
